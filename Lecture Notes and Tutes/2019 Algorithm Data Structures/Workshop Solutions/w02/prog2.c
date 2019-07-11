0d7994668ad65a0e265e127fde21b0cf3abb3ed4
Akira Wang <30443254+akiratwang@users.noreply.github.com>
2019-07-11 21:03:09 +1000

    started w02 stuff

---
+++ Lecture Notes and Tutes/2019 Algorithm Data Structures/Workshop Solutions/w02/prog2.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+int main(int argc, char **argv){
+    int numStrings;
+    int nextStringLength;
+    int i, j;
+    char **strings;
+    
+    /* Read number of strings to store. */
+    scanf("%d", &numStrings);
+    
+    /* Allocate space for the array of strings. */
+    *strings = (char**)calloc(numStrings, sizeof(char*));
+    
+    /* For each string, get its length, allocate space for it
+        and read all the characters into the string.
+        Note: Remember, these are standard strings. */
+    for (i = 0; i < numStrings; i++) {
+        scanf("%d", &nextStringLength);
+        strings[i] = (char*)calloc(nextStringLength + 1, sizeof(char));
+        for (j = 0; j < nextStringLength; j++) {
+            scanf("%c",strings[i][j]);
+        }
+        strings[i][nextStringLength] = '\0';
+    }
+    
+    
+    /* The below prints all words in the array with spaces between them. */
+    if(numStrings > 0){
+        printf("%s",strings[0]);
+    }
+    
+    for(i = 1; i < numStrings; i++){
+        printf(" %s",strings[i]);
+    }
+    printf("\n");
+
+    for (i = 0; i < numStrings; i++) {
+        free(strings[i])
+    }
+    free(strings)
+    
+    return 0;
+}
